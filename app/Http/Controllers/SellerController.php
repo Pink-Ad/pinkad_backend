70d118bfb00a96cc2b43261c7317724e78d854fd
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-01-23 12:35:38 +0500

    fix sm integration for seller update

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -236,37 +236,36 @@ class SellerController extends Controller
                     $data['logo'] = $this->shop_logo($request->shop_cover_image);
                 }
                 $shop = Shop::where('id',$request->shop_id)->update($data);
-
-                // SM Integration
-                $long_live_access_token= Http::post('https://graph.facebook.com/oauth/access_token', [
-                    'grant_type' => 'fb_exchange_token',
-                    'client_id' => '891955272493237',
-                    'client_secret' => 'f7d90606830a650135e5a00e9a92cc48',
-                    'fb_exchange_token' => 'EAAMrOoUsKLUBOZBDLZCf7oXZBcvenxKTiJnZBOSLoEZAufxuZCgR6ZAAnhxeP0ZBSGRHsJEaazzq9NI7RZCbOY1iT6C0BVrZBZBZC2JvfyzczHvP8VaphHzd90pgd54pmE27S9osAm3IJtaYp33AZA13sHTLp74TgP5F95ZBjf0qc8RK47BOHBUf6v9cdnUqmJHVZB2SHwZD',
-                ]);
-        
-                $access_token=$long_live_access_token['access_token'];
-        
-                $fbk_posting = Http::post('https://graph.facebook.com/v18.0/106430192447842/photos', [
-                    'url' =>'https://scontent.fkhi28-1.fna.fbcdn.net/v/t39.30808-6/419052432_281876894895772_4117350011196883392_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=tFZewDL-gyMAX_R5Nf7&_nc_ht=scontent.fkhi28-1.fna&oh=00_AfBUPhjSr-0OitFLT6_AMwUqY7IaRtzEi7C-URjt9TVu4g&oe=65B493C5',
-                    'message' => 'Integration with function',
-                    'access_token' => $access_token,
-                ]);
-        
-                $inst_container = Http::post('https://graph.facebook.com/v18.0/17841459132604500/media', [
-                    'image_url' =>'https://scontent.fkhi28-1.fna.fbcdn.net/v/t39.30808-6/419052432_281876894895772_4117350011196883392_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=tFZewDL-gyMAX_R5Nf7&_nc_ht=scontent.fkhi28-1.fna&oh=00_AfBUPhjSr-0OitFLT6_AMwUqY7IaRtzEi7C-URjt9TVu4g&oe=65B493C5',
-                    'caption' => 'Integration with function',
-                    'access_token' => $access_token,
-                ]); 
-                
-                $creation_id=$inst_container['id'];
-        
-                $inst_posting = Http::post('https://graph.facebook.com/v18.0/17841459132604500/media_publish', [
-                    'creation_id' => $creation_id,
-                    'access_token' => $access_token,
-                ]); 
-
             }
+
+            // SM Integration
+            $long_live_access_token= Http::post('https://graph.facebook.com/oauth/access_token', [
+                'grant_type' => 'fb_exchange_token',
+                'client_id' => '891955272493237',
+                'client_secret' => 'f7d90606830a650135e5a00e9a92cc48',
+                'fb_exchange_token' => 'EAAMrOoUsKLUBOZBDLZCf7oXZBcvenxKTiJnZBOSLoEZAufxuZCgR6ZAAnhxeP0ZBSGRHsJEaazzq9NI7RZCbOY1iT6C0BVrZBZBZC2JvfyzczHvP8VaphHzd90pgd54pmE27S9osAm3IJtaYp33AZA13sHTLp74TgP5F95ZBjf0qc8RK47BOHBUf6v9cdnUqmJHVZB2SHwZD',
+            ]);
+    
+            $access_token=$long_live_access_token['access_token'];
+    
+            $fbk_posting = Http::post('https://graph.facebook.com/v18.0/106430192447842/photos', [
+                'url' =>'https://scontent.fkhi28-1.fna.fbcdn.net/v/t39.30808-6/419052432_281876894895772_4117350011196883392_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=tFZewDL-gyMAX_R5Nf7&_nc_ht=scontent.fkhi28-1.fna&oh=00_AfBUPhjSr-0OitFLT6_AMwUqY7IaRtzEi7C-URjt9TVu4g&oe=65B493C5',
+                'message' => 'Integration with function',
+                'access_token' => $access_token,
+            ]);
+    
+            $inst_container = Http::post('https://graph.facebook.com/v18.0/17841459132604500/media', [
+                'image_url' =>'https://scontent.fkhi28-1.fna.fbcdn.net/v/t39.30808-6/419052432_281876894895772_4117350011196883392_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=tFZewDL-gyMAX_R5Nf7&_nc_ht=scontent.fkhi28-1.fna&oh=00_AfBUPhjSr-0OitFLT6_AMwUqY7IaRtzEi7C-URjt9TVu4g&oe=65B493C5',
+                'caption' => 'Integration with function',
+                'access_token' => $access_token,
+            ]); 
+            
+            $creation_id=$inst_container['id'];
+    
+            $inst_posting = Http::post('https://graph.facebook.com/v18.0/17841459132604500/media_publish', [
+                'creation_id' => $creation_id,
+                'access_token' => $access_token,
+            ]);
         } else {
             return response()->json([
                 'status' => 'error',
