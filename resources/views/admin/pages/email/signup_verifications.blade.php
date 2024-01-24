d958bc8b0c93596cb887945827892b9ea0e68e62
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2024-01-25 00:12:25 +0500

    fixes

--- resources/views/admin/pages/email/signup_verifications.blade.php
+++ resources/views/admin/pages/email/signup_verifications.blade.php
@@ -136,7 +136,7 @@
                          Just press the button below.</p>
                     </td>
                 </tr>
-                <tr>
+                <!-- <tr>
                     <td bgcolor="#ffffff" align="left">
                         <table width="100%" border="0" cellspacing="0" cellpadding="0">
                             <tr>
@@ -157,7 +157,8 @@
                             </tr>
                         </table>
                     </td>
-                </tr> <!-- COPY -->
+                </tr>  -->
+                <!-- COPY -->
                 <!-- <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                         <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
@@ -203,6 +204,28 @@
                 </p>
                     </td>
                 </tr>
+                <tr>
+                    <td bgcolor="#ffffff" align="left">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
+                                    <table border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B">
+                                            <a href="https://app.pinkad.pk/email-verified" target="_blank" 
+                                            style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff;
+                                             text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; 
+                                             border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">
+                                             Click Here to Verify Your Email
+                                             </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr> 
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                         <p style="margin: 0;">Pinkad Team</p>
