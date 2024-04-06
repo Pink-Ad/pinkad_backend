7b8f329174b34ec0a966c8411d0aa843bd31579f
hassansmirza12 <162713769+hassansmirza12@users.noreply.github.com>
2024-04-06 08:01:07 -0700

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -204,7 +204,7 @@ class PostController extends Controller
         $post = Post::with('shop', 'shop.seller', 'category', 'subcategory')
         ->where('status', 1)
         ->where('IsFeature', 1)
-        ->OrderBy('id', 'DESC')->paginate(600);
+        ->OrderBy('id', 'DESC')->paginate(99999);
         // $post = Post::with(['shop', 'shop.seller', 'category', 'subcategory'])
         // ->where('status', 1)
         // ->where('IsFeature', 1)
