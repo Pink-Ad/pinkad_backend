1defe3ed5e93e92243e8e24d57e923d55f00b8f9
hassansmirza12 <162713769+hassansmirza12@users.noreply.github.com>
2024-05-29 12:07:17 -0700

    fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -782,12 +782,12 @@ class AuthController extends Controller
              // URL-encode the email
              $encodedEmail = urlencode($request->email);
              // 
-            $data1['verify_token'] = "http://ms-hostingladz.com/DigitalBrand/email/verify/".$request->email."/".$verify_token;
+            // $data1['verify_token'] = "http://ms-hostingladz.com/DigitalBrand/email/verify/".$request->email."/".$verify_token;
             $cmd = DB::connection('mysql')->table('users')
                 ->where('email', $request->email)
                 ->update(['remember_token' => $verify_token, 'updated_at' => Carbon::now()]);
                 // 
-            $data1['verify_token_email'] = 'https://qa.pinkad.pk/api/usermail/email_verified/' . $encodedEmail.'/'.$verify_token;
+            $data1['verify_token'] = 'https://qa.pinkad.pk/api/usermail/email_verified/' . $encodedEmail.'/'.$verify_token;
                 // 
             $data1['email'] = $request->email;
             Mail::send('admin.pages.email.signup_verifications',['data' => $data1], function ($message)use($data1) {
